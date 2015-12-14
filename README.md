9a379be01c84b0aa1ffc45d127dd46b5735e754e
Bruno Bonnin <bruno.bonnin@myscript.com>
2015-12-14 12:35:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,67 @@
+## Mongo2Els
+
+Tool for copying data from MongoDB to Elasticsearch.
+
+### Build
+
+Requirements:
+* Maven 3
+* Java 8
+
+```bash
+mvn clean package
+```
+
+### Run
+
+Arguments:
+* -m : MongoDB connection URL (host:port)
+* -d : MongoDB database
+* -c : MongoDB collection
+* -q : MongoDB query (to select data)
+* -p : MongoDB query projection (to select fields to be indexed in Elasticsearch)
+* -e : Elasticsearch connection URL (host:port)
+* -i : Elasticsearch index (by default = db name)
+* -t : Elasticsearch document type (by default = collection name)
+
+Run with the default arguments:
+
+```bash
+java -jar mongo2els-jar-with-dependencies.jar
+
+2015-12-13 12:28:05.047 [main] INFO  io.millesabords.mongo2els.Mongo2Els - Config: Mongo2Els:
+  MongoDB url='localhost:27017'
+  MongoDB database name='test'
+  MongoDB collection name='test'
+  MongoDB query='{}'
+  MongoDB projection='null'
+  Elasticsearch url='localhost:9300'
+  Elasticsearch index='test'
+  Elasticsearch doc type='test'
+```
+
+
+Run with arguments:
+
+```bash
+
+java -jar mongo2els-jar-with-dependencies.jar -m localhost:27100 -e localhost:10300 -d mydb -c mycollection -p "{'fileName':1}" -i myindex -t mytype -q "{'path':'/etc'}"
+
+2015-12-13 12:32:46.531 [main] INFO  io.millesabords.mongo2els.Mongo2Els - Config: Mongo2Els:
+  MongoDB url='localhost:27100'
+  MongoDB database name='mydb'
+  MongoDB collection name='mycollection'
+  MongoDB query='{'path':'/etc'}'
+  MongoDB projection='{'fileName':1}'
+  Elasticsearch url='localhost:10300'
+  Elasticsearch index='myindex'
+  Elasticsearch doc type='mytype'
+
+```
+
+### Env
+
+This tool has been tested with:
+* Elasticsearch 2.1.0
+* MongoDB 3.0.7
+
---
+++ src/main/java/io/millesabords/mongo2els/Mongo2Els.java
@@ -0,0 +1,169 @@
+package io.millesabords.mongo2els;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
+import org.jongo.MongoCursor;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * Simple tool for exporting data from MongoDB to Elasticsearch.
+ *
+ * @author Bruno Bonnin
+ */
+public class Mongo2Els {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(Mongo2Els.class);
+
+    private static int BULK_SIZE = 1000;
+
+    @Option(name = "-m", usage = "MongoDB connection URL (host:port)")
+    private String mongodbUrl = "localhost:27017";
+
+    @Option(name = "-d", usage = "MongoDB database")
+    private String dbName = "test";
+
+    @Option(name = "-c", usage = "MongoDB collection")
+    private String collectionName = "test";
+
+    @Option(name = "-q", usage = "MongoDB query")
+    private String query = "{}";
+
+    @Option(name = "-p", usage = "MongoDB query projection")
+    private String projection = null;
+
+    @Option(name = "-e", usage = "Elasticsearch connection URL (host:port)")
+    private String elsUrl = "localhost:9300";
+
+    @Option(name = "-i", usage = "Elasticsearch index (by default = db name)")
+    private String index = null;
+
+    @Option(name = "-t", usage = "Elasticsearch document type (by default = collection name)")
+    private String doctype = null;
+
+    private MongoClient mongoClient;
+
+    private Jongo jongo;
+
+    private Client elsClient;
+
+    private int total = 0;
+
+
+    public static void main(String[] args) {
+        new Mongo2Els().doMain(args);
+    }
+
+    private void doMain(String[] args) {
+        final CmdLineParser parser = new CmdLineParser(this);
+
+        try {
+            parser.parseArgument(args);
+
+            if (index == null) {
+                index = dbName;
+            }
+            if (doctype == null) {
+                doctype = collectionName;
+            }
+
+            LOGGER.info("Config: {}", this);
+
+            initMongoClient();
+            initElsClient();
+
+            LOGGER.info("Start : {}", new Date());
+
+            transferData();
+        }
+        catch (CmdLineException e) {
+            System.err.println("Bad argument");
+            parser.printUsage(System.err);
+        }
+        catch (Exception e) {
+            LOGGER.error("Problem during processing", e);
+        }
+
+        LOGGER.info("End : {}", new Date());
+        LOGGER.info("Total of indexed docs : {}", total);
+    }
+
+    private void initMongoClient() {
+        mongoClient = new MongoClient(new MongoClientURI("mongodb://" + mongodbUrl));
+        mongoClient.getServerAddressList(); // To check that the connection is ok
+        jongo = new Jongo(mongoClient.getDB(dbName));
+    }
+
+    private void initElsClient() throws IOException {
+        final String host = elsUrl.substring(0, elsUrl.indexOf(":"));
+        final int port = Integer.parseInt(elsUrl.substring(elsUrl.indexOf(":") + 1));
+
+        final Settings settings = Settings.settingsBuilder()
+            .put("client.transport.ignore_cluster_name", true).build();
+
+        elsClient = TransportClient.builder().settings(settings).build()
+            .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), port));
+    }
+
+    /**
+     * Data transfer from MongoDB to Elasticsearch.
+     * It uses a bulk indexation.
+     */
+    private void transferData() {
+        final BulkRequestBuilder bulkRequest = elsClient.prepareBulk();
+        final MongoCollection collection = jongo.getCollection(collectionName);
+        final MongoCursor<Map> cursor = collection.find(query).projection(projection).as(Map.class);
+        Map doc;
+        BulkResponse bulkResponse;
+        String id;
+        int count = 0;
+
+        while (cursor.hasNext()) {
+            total++;
+            count++;
+            doc = cursor.next();
+            id = doc.remove("_id").toString();
+            bulkRequest.add(elsClient.prepareIndex(index, doctype, id).setSource(doc));
+            if (count % BULK_SIZE == 0 || !cursor.hasNext()) {
+                LOGGER.info("Indexing {} docs...", count);
+                bulkResponse = bulkRequest.get();
+                if (bulkResponse.hasFailures()) {
+                    System.err.println("ERROR : problems occur with the bulk request: " + bulkResponse.buildFailureMessage());
+                }
+                LOGGER.info("Current total : {}", total);
+                count = 0;
+            }
+
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Mongo2Els:\n" +
+                "  MongoDB url='" + mongodbUrl + "'\n" +
+                "  MongoDB database name='" + dbName + "'\n" +
+                "  MongoDB collection name='" + collectionName + "'\n" +
+                "  MongoDB query='" + query + "'\n" +
+                "  MongoDB projection='" + projection + "'\n" +
+                "  Elasticsearch url='" + elsUrl + "'\n" +
+                "  Elasticsearch index='" + index + "'\n" +
+                "  Elasticsearch doc type='" + doctype + "'\n";
+    }
+}
